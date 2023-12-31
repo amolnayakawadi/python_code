92296e35410c821c32c9a4f60389b8572299ee80
Amol Nayakawadi <amolnayakawadi1@gmail.com>
2023-12-31 23:20:52 +0530

    first commit

---
+++ base.py
@@ -0,0 +1,35 @@
+## Variable = variable is placeholder where address of the data is stored
+
+age = '5'
+print(age)
+
+
+## conditions 
+
+add = 5 + 5
+print(add)
+
+sub = 7 - 5
+print(sub)
+
+mult = 5 * 8
+print(mult)
+
+div = 6 / 3
+print(div)
+
+
+## Remainder
+
+info = 6 % 2
+print(info)
+
+
+## Exponential
+
+data = 5**2
+print(data)
+
+
+expo = 6**3
+print(expo)
