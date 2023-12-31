7e99db796fd35954ccbb92a335f8b0cd8bebb79a
Amol Nayakawadi <amolnayakawadi1@gmail.com>
2023-12-31 23:07:46 +0530

    comparisons commit

---
+++ file.py
@@ -0,0 +1,22 @@
+### Comparisons 
+
+print(4 == 5)
+
+print(3 != 4)
+
+print(6 <= 7)
+
+print(9 >= 2)
+
+
+## in , not in
+
+print('a' in 'amol')
+
+print('N' not in 'python')
+
+
+## Identity
+
+list1 = [1,2,3,4,5]
+print(id(list1))
